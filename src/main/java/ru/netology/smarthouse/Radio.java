054ece0d9cac8eca55c27d54775ec244da240e54
Dasha <dashuta.tkachenko@list.ru>
2021-12-02 22:49:19 +0300

    first commit

---
+++ src/main/java/ru/netology/smarthouse/Radio.java
@@ -0,0 +1,61 @@
+package ru.netology.smarthouse;
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+    private int newCurrentStation;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (newCurrentStation < 0) {
+            newCurrentStation = 0;
+        }
+        if (newCurrentStation > 9) {
+            newCurrentStation = 9;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void nextStation() {
+        if (currentStation < 9) {
+            currentStation += 1;
+        } else {
+            currentStation = 0;
+        }
+    }
+    public void prevStation() {
+        if (currentStation > 0) {
+            currentStation = currentStation - 1;
+        }
+        if (currentStation == 0) {
+        } else {
+            currentStation = 9;
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+    public void setCurrentVolume (int newVolume) {
+        if (newVolume > 10) {
+            newVolume = 10;
+        }
+        currentVolume = newVolume;
+    }
+    public void plusVolume() {
+        if (currentVolume < 10) {
+            currentVolume += 1;
+        }
+    }
+    public void minusVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+}
+
+
+
